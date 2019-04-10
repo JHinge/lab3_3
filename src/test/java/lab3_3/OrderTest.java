b1d81168c8bc613e3a59cddb29784755aa506060
Zawiasa.Jakub <zawiasa.jakub@gmail.com>
2019-04-10 18:17:57 +0200

    FakeDateTime created

---
+++ src/test/java/lab3_3/OrderTest.java
@@ -0,0 +1,15 @@
+package lab3_3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class OrderTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
