8cd43121cbe8e2b30b554886c8441f6d7358854d
Zawiasa.Jakub <zawiasa.jakub@gmail.com>
2019-04-10 18:48:55 +0200

    shouldNotMarkOrderAsCancelled24hoursAfterSubmission

--- src/test/java/lab3_3/OrderTest.java
+++ src/test/java/lab3_3/OrderTest.java
@@ -1,5 +1,9 @@
 package lab3_3;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 
 import edu.iis.mto.time.FakeDateTime;
@@ -19,4 +23,15 @@ public class OrderTest {
         order.confirm();
     }
 
+    @Test
+    public void shouldNotMarkOrderAsCancelled24hoursAfterSubmission() {
+        FakeDateTime fakeDateTime = new FakeDateTime();
+        fakeDateTime.addDateToReturn(2019, 10, 1, 0, 0);
+        fakeDateTime.addDateToReturn(2019, 10, 2, 0, 0);
+
+        Order order = new Order(fakeDateTime);
+        order.submit();
+        order.confirm();
+        assertThat(order.getOrderState(), not(equalTo(Order.State.CANCELLED)));
+    }
 }
