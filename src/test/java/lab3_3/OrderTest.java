8bf648cf965e41863f4be8328d93c962bf49bc87
Zawiasa.Jakub <zawiasa.jakub@gmail.com>
2019-04-10 18:42:22 +0200

    shouldThrowOrderExpiredExceptionIfTimeForSubmitExpired

--- src/test/java/lab3_3/OrderTest.java
+++ src/test/java/lab3_3/OrderTest.java
@@ -1,15 +1,22 @@
 package lab3_3;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+import edu.iis.mto.time.FakeDateTime;
+import edu.iis.mto.time.Order;
+import edu.iis.mto.time.OrderExpiredException;
 
 public class OrderTest {
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
+    @Test(expected = OrderExpiredException.class)
+    public void shouldThrowOrderExpiredExceptionIfTimeForSubmitExpired() {
+        FakeDateTime fakeDateTime = new FakeDateTime();
+        fakeDateTime.addDateToReturn(2019, 2, 3, 6, 6);
+        fakeDateTime.addDateToReturn(2019, 6, 8, 6, 6);
+
+        Order order = new Order(fakeDateTime);
+        order.submit();
+        order.confirm();
     }
 
 }
