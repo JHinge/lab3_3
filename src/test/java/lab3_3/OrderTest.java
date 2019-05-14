b4084cf55aa574224f13e20dac2dc2e2ad58f004
Zawiasa.Jakub <zawiasa.jakub@gmail.com>
2019-05-14 16:08:49 +0200

    test refactor

--- src/test/java/lab3_3/OrderTest.java
+++ src/test/java/lab3_3/OrderTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
-import edu.iis.mto.time.FakeDateTime;
+import edu.iis.mto.time.AdjustableDateTime;
 import edu.iis.mto.time.Order;
 import edu.iis.mto.time.OrderExpiredException;
 
@@ -14,22 +14,22 @@ public class OrderTest {
 
     @Test(expected = OrderExpiredException.class)
     public void shouldThrowOrderExpiredExceptionIfTimeForSubmitExpired() {
-        FakeDateTime fakeDateTime = new FakeDateTime();
-        fakeDateTime.addDateToReturn(2019, 2, 3, 6, 6);
-        fakeDateTime.addDateToReturn(2019, 6, 8, 6, 6);
+        AdjustableDateTime adjustableDateTime = new AdjustableDateTime();
+        adjustableDateTime.addDateToReturn(2019, 2, 3, 6, 6);
+        adjustableDateTime.addDateToReturn(2019, 6, 8, 6, 6);
 
-        Order order = new Order(fakeDateTime);
+        Order order = new Order(adjustableDateTime);
         order.submit();
         order.confirm();
     }
 
     @Test
     public void shouldNotMarkOrderAsCancelled24hoursAfterSubmission() {
-        FakeDateTime fakeDateTime = new FakeDateTime();
-        fakeDateTime.addDateToReturn(2019, 10, 1, 0, 0);
-        fakeDateTime.addDateToReturn(2019, 10, 2, 0, 0);
+        AdjustableDateTime adjustableDateTime = new AdjustableDateTime();
+        adjustableDateTime.addDateToReturn(2019, 10, 1, 0, 0);
+        adjustableDateTime.addDateToReturn(2019, 10, 2, 0, 0);
 
-        Order order = new Order(fakeDateTime);
+        Order order = new Order(adjustableDateTime);
         order.submit();
         order.confirm();
         assertThat(order.getOrderState(), not(equalTo(Order.State.CANCELLED)));
@@ -37,11 +37,11 @@ public class OrderTest {
 
     @Test
     public void shouldConfirmOrderIfNotExpired() {
-        FakeDateTime fakeDateTime = new FakeDateTime();
-        fakeDateTime.addDateToReturn(2019, 10, 1, 0, 0);
-        fakeDateTime.addDateToReturn(2019, 10, 1, 2, 30);
+        AdjustableDateTime adjustableDateTime = new AdjustableDateTime();
+        adjustableDateTime.addDateToReturn(2019, 10, 1, 0, 0);
+        adjustableDateTime.addDateToReturn(2019, 10, 1, 2, 30);
 
-        Order order = new Order(fakeDateTime);
+        Order order = new Order(adjustableDateTime);
         order.submit();
         order.confirm();
         assertThat(order.getOrderState(), not(equalTo(Order.State.CONFIRMED)));
